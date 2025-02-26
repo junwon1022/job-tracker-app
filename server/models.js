42f5b801948993c8d36faef8cdb4500d59080efa
Junwon <junwon1022@naver.com>
2025-02-26 14:32:50 +0100

    Added model for user and routes for user authentication

--- server/models.js
+++ server/models.js
@@ -1,1 +1,25 @@
- 
+const mongoose = require('mongoose');
+
+// Define the User Schema (Database Structure)
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Export the model so we can use it in other files
+module.exports = mongoose.model('User', UserSchema);
