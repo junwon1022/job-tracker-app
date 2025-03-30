5a48d487cf3fb99cfd8f11677a7d54f43aac6528
Junwon <junwon1022@naver.com>
2025-03-30 17:44:01 +0200

    Remove unused import

--- client/src/pages/dropdown/friends.tsx
+++ client/src/pages/dropdown/friends.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../navbar";
 import "../../styles/dropdown/friends.css";
 import defaultAvatar from "../../assets/user.png";
-import Modal from "../modal/modal"; 
 import ConfirmModal from "../modal/confirmModal";
 
 const Friends = () => {
